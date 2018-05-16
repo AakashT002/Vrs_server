1182d25e045fbe20ce2fbb56002bf14d8c05d8d5
Tirole <568538@cognizant.com>
2018-05-16 15:21:53 +0530

    gtins in lookup service updated

--- services/LookupService.js
+++ services/LookupService.js
@@ -5,8 +5,8 @@ const lookupDirectory = new LookupDirectory(process.env.NODE_ADDRESS);
 
 module.exports = {
 	gtins: {
-		'10505801235015::CognizantVRS': { endpoint : 'https://lp02-team-m-responder-server.herokuapp.com', requestType: constants.CI_TYPE_REST_ENDPOINT, entityType:constants.ENTITY_TYPE_MANUFACTURER , entityId:'Pfizer'},
-		'30367534281200::CognizantVRS': { endpoint : 'https://lp02-team-m-responder-server.herokuapp.com', requestType: constants.CI_TYPE_REST_ENDPOINT, entityType:constants.ENTITY_TYPE_MANUFACTURER , entityId:'Pfizer'},
+		'00300028799598::CognizantVRS': { endpoint : 'https://lp02-team-m-responder-server.herokuapp.com', requestType: constants.CI_TYPE_REST_ENDPOINT, entityType:constants.ENTITY_TYPE_MANUFACTURER , entityId:'Pfizer'},
+		'00301696438109::CognizantVRS': { endpoint : 'https://lp02-team-m-responder-server.herokuapp.com', requestType: constants.CI_TYPE_REST_ENDPOINT, entityType:constants.ENTITY_TYPE_MANUFACTURER , entityId:'Pfizer'},
 		'10350881006602::CognizantVRS': { endpoint : 'https://lp02-team-m-other-vrs-server.herokuapp.com', requestType: constants.CI_TYPE_REST_ENDPOINT, entityType:constants.ENTITY_TYPE_VRS_PROVIDER , entityId:'OtherVRS'},
 		'10350881006602::OtherVRS': { endpoint : 'https://lp02-team-m-responder-server.herokuapp.com', requestType: constants.CI_TYPE_REST_ENDPOINT, entityType:constants.ENTITY_TYPE_MANUFACTURER , entityId:'Pfizer'},
 		'10350881234567::CognizantVRS': { endpoint : 'http://lp02-team-m-third-vrs-server.herokuapp.com', requestType: constants.CI_TYPE_REST_ENDPOINT, entityType:constants.ENTITY_TYPE_VRS_PROVIDER , entityId:'VRS3'},
